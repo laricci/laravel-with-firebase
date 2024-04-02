19736ec73cd939718b8fcd4f7db17ffc166db952
Laura Ricci <lauraricci.lrc@gmail.com>
2024-04-02 11:20:25 -0300

    Add Firebase Controller

---
+++ app/Http/Controllers/FirebaseController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FirebaseController extends Controller
+{
+    protected $database;
+
+    public function __construct()
+    {
+        $this->database = app('firebase.database');
+    }
+
+    public function index()
+    {
+        echo 'index';
+    }
+}
